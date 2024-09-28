eeeb218977c534cef15f3c58fdb70a1abaaa53bd
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-09-28 12:40:22 +0530

    28-09-2024 all data updated

---
+++ module-2-javascript-basics/what is function in javascript/types_of_function/normal_function/js/adressess.js
@@ -0,0 +1,23 @@
+function add1()
+{
+ var ad1="150 feet ring road Behind Raiya swaminarayn Hall Road -360005";
+ document.write(ad1);
+}
+function add2()
+{
+    
+ var ad2="150 feet ring road Behind Indira circle Nyara Petrol pump Road -360001";
+ document.write(ad2);
+}
+function add3()
+{
+    
+ var ad3="Jalaram plot-2 behind indira circle Road -360005";
+ document.write(ad3);
+}
+function add4()
+{
+    
+ var ad4="Near airport road amazone Hall Road -360005";
+ document.write(ad4);
+}
---
+++ module-2-javascript-basics/what is javascript variables data types/undefined.js
@@ -0,0 +1,11 @@
+/* undefined datatypes
+  var name;
+  var a;
+  var b;
+
+
+*/
+
+
+var name;
+console.log(name);
